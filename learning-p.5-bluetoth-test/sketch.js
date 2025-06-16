000746ad49276cb2986a741968b58f0089a02262
Skeana <skean2000@outlook.com>
2025-06-16 16:01:16 +0100

    traiangle working

---
+++ learning-p.5-bluetoth-test/sketch.js
@@ -0,0 +1,19 @@
+// This is where your p5.js code from the video goes
+// For example, from the video:
+
+function setup() {
+  createCanvas(500, 500); // 400 width, 300 height as in the video
+  // You might want to uncomment and try rectMode here later:
+  // rectMode(CENTER);
+}
+
+function draw() {
+  background(220, 200, 200); // A light pink background, similar to the video
+  // The rectangle
+  
+  rect(130, 75, 150, 150, 20);
+  triangle(130, 175, 158, 120, 186, 175);
+}
+
+
+// this is the starter video that i am watching to learn (https://www.youtube.com/watch?v=c3TeLi6Ns1E)
