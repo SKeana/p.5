44a3cbd375ab6b9d2879c7acb49a64912e888b3e
Skeana <skean2000@outlook.com>
2025-06-22 17:27:17 +0100

    learn that different why of doing the anamatyion

--- learning-p.5-lesson-3/sketch.js
+++ learning-p.5-lesson-3/sketch.js
@@ -1,22 +1,22 @@
 function setup() {
-  createCanvas(500, 500); 
+  createCanvas(500, 500);
+  background(225, 0, 0); 
 }
 
 function draw() {
-  background(225, 0, 0); // A light pink background, similar to the video
   fill(0, 0, 225);
-  stroke(255, 225, 225);
-  strokeWeight(1);
-  rect(130, 105, 150, 150, 20);
-  stroke(100);
-  strokeWeight(2);
-  fill(0, 0, 0, 150);
-  triangle(230, 275, 258, 220, 286, 275);
+  noStroke();
+  rect(mouseX, mouseY, 24);
+ 
 }
 
 
-// this watching to learn (https://www.youtube.com/watch?v=LuGsp5KeJMM)
+// this watching to learn (https://www.youtube.com/watch?v=7A5tKW9HGoM)
 /*
-
+  When looking at the way that the function are set up setup is only done onese and first that why we put canvas there but draaw on the other hand happen all the time
+  and the order is from top to bottom then back to the top and this loop will happen untill you stop the program.
+  there are times when you want you shap to follow the mose and this is why p5 give you a var call mouse the it can follow x or y.
+  when trying to cover the background with what the anamation you are running is because the background is at the top and getting draw agin and agin, but moving it to the
+  setup part means it will not change.
 
 */
