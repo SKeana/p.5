79fd81c0b340eb5896f7d73a90912a020f301755
Skeana <skean2000@outlook.com>
2025-06-26 17:11:27 +0100

    finish lesson and set up next lesson

--- learning-p.5-lesson-4/sketch.js
+++ learning-p.5-lesson-4/sketch.js
@@ -13,6 +13,7 @@ function draw() {
 
 // this watching to learn (https://www.youtube.com/watch?v=dRhXIIFp-ys)
 /*
- 
+ when making your var you need to start with the key word "let", then you need to initialize it meaning you give it a starting off point doing this is call assiginget 
+ oporation think it as you have a bucket and you put in the value 100, then you use it as it would be pointless to not 
 
 */
---
+++ learning-p.5-lesson-5/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 500);
+  background(225, 0, 0); 
+}
+
+function draw() {
+  fill(0, 0, 225);
+  noStroke();
+  rect(mouseX, mouseY, 24);
+ 
+}
+
+
+// this watching to learn (https://www.youtube.com/watch?v=T26OJGjI8qI)
+/*
+ 
+
+*/
